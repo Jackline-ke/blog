d91c8f032afe8f094eb1eadfe82491a0721893fd
Jackline-ke <antoninahodhiambo@gmail.com>
2024-01-16 16:08:19 +0300

    files

--- client/src/pages/Write.js
+++ client/src/pages/Write.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Write = () => {
   return (
-    <div>
-        
+    <div className='font-serif w-full px-8 lg:px-16 flex flex-col items-center justify-center' style={{ paddingTop: '80px' }}>
+        write
     </div>
   )
 }
